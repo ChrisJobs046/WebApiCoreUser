b1c2ab3d1a8a64501ede5a2f3b03403d30c676c3
ChrisJobs046 <ChrisJobs046@gmail.com>
2021-06-10 19:51:50 -0400

    WebApi para Usuarios

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace WebApiAuth.Models
+{
+    public partial class User
+    {
+        public int AuthId { get; set; }
+        public string FistName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime? EnrollmentDate { get; set; }
+    }
+}
